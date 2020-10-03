1b84757807e3f15a16e6d891163e448617f693ae
DevDemo-stack <dzone778@gmail.com>
2020-10-03 15:40:36 +0530

    update Icon events

--- Index.js
+++ Index.js
@@ -1,3 +1,20 @@
 function openLink() {
-    window.open("https://twitter.com/?lang=en-in");
+    if ('twitter') {
+        window.open("https://twitter.com/?lang=en-in");
+    }
+    if ('apple') {
+        window.open("https://www.apple.com/in/");
+    }
+    if ('facebook') {
+        window.open("https://www.facebook.com/");
+    }
+    if ('google_drive') {
+        window.open("https://accounts.google.com/ServiceLogin/signinchooser?service=wise&passive=1209600&continue=https%3A%2F%2Fdrive.google.com%2F&followup=https%3A%2F%2Fdrive.google.com%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+    }
+    if ('youtube') {
+        window.open("https://www.youtube.com/?gl=IN");
+    }
+    if ('instagram') {
+        window.open("https://www.instagram.com/?hl=en");
+    }
 }
