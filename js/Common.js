9da1e116144f3f6d48fc6c2e2b125a76ba6972ea
DevDemo-stack <dzone778@gmail.com>
2020-10-25 14:23:36 +0530

    Add Common js remove commented code.

---
+++ js/Common.js
@@ -0,0 +1,20 @@
+function openLink(link) {
+    if (link=="twitter") {
+        window.open("https://twitter.com/?lang=en-in");
+    }
+    if (link=="apple") {
+        window.open("https://www.apple.com/in/");
+    }
+    if (link=="facebook") {
+        window.open("https://www.facebook.com/");
+    }
+    if (link=="google_drive") {
+        window.open("https://accounts.google.com/ServiceLogin/signinchooser?service=wise&passive=1209600&continue=https%3A%2F%2Fdrive.google.com%2F&followup=https%3A%2F%2Fdrive.google.com%2F&emr=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
+    }
+    if (link=="youtube") {
+        window.open("https://www.youtube.com/?gl=IN");
+    }
+    if (link=="instagram") {
+        window.open("https://www.instagram.com/?hl=en");
+    }
+}
